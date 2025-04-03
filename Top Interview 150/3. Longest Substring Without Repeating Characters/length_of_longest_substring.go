a04e63a99d095846d7a3fedb14b19c0a3dd75490
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-04-03 14:18:54 +0300

    Update length_of_longest_substring.go

--- Top Interview 150/3. Longest Substring Without Repeating Characters/length_of_longest_substring.go
+++ Top Interview 150/3. Longest Substring Without Repeating Characters/length_of_longest_substring.go
@@ -1,1 +1,21 @@
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
 
+func lengthOfLongestSubstring(s string) int {
+    set := make(map[byte]bool)
+    maxLen, leftMost := 0, 0
+
+    for right := 0; right < len(s); right++ {
+        for set[s[right]] {
+            delete(set, s[leftMost])
+            leftMost++
+        }
+        set[s[right]] = true
+        maxLen = max(maxLen, len(set))
+    }
+    return maxLen
+}
