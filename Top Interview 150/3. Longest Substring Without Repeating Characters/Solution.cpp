2a66716bb778680a1603d7b5befdf80110e8d71b
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-04-03 13:58:10 +0300

    Update Solution.cpp

--- Top Interview 150/3. Longest Substring Without Repeating Characters/Solution.cpp
+++ Top Interview 150/3. Longest Substring Without Repeating Characters/Solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) 
-    {
+    int lengthOfLongestSubstring(string str) {
         unordered_set<char> s;
         int maxLen=0;
         int leftmost=0;
-        for(int i = 0; i < str.size(); i++)
-        {
-            if(s.find(str[i])!=s.end())
-            {
-                while(s.find(str[i])!=s.end())
-                {
-                    s.erase(str[leftmost++]);
-                }
+        for(int i = 0; i < str.size(); i++) {
+            while(s.find(str[i])!=s.end()) {
+                s.erase(str[leftmost++]);
             }
             s.insert(str[i]);
             maxLen=max(maxLen, (int)s.size());
