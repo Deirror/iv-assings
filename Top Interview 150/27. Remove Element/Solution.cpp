e675fcabd32e9eb3ee5baa088603b4e4f6371058
Alex Angelow <136594831+Deirror@users.noreply.github.com>
2025-04-03 11:46:50 +0300

    Update Solution.cpp

--- Top Interview 150/27. Remove Element/Solution.cpp
+++ Top Interview 150/27. Remove Element/Solution.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != val) {
+                if(i == k) {
+                    k++;
+                    continue;
+                }
+                swap(nums[i], nums[k]);
+                k++;
+            }
+        }
+        return k;
+    }
+};
